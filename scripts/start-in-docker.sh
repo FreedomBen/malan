88a761e6b069e2be1adf31f3891d250e20e955f2
Benjamin Porter <FreedomBen@users.noreply.github.com>
2021-04-21 16:54:50 -0600

    Add check for Postgres initializatoin to startup script in docker

--- scripts/start-in-docker.sh
+++ scripts/start-in-docker.sh
@@ -1,7 +1,22 @@
 #!/usr/bin/env bash
 
+set -e
+
+# Wait for Postgres to initialize
+echo "[*] Waiting for PostgreSQL to initialize..."
+
+while ! nc -z db 5432; do
+  sleep 0.1
+done
+
+echo "[*] PostgreSQL responded"
+
 if [[ "$INIT_DB" =~ [yY] ]]; then
+  echo "[*] INIT_DB is set to '$INIT_DB'.  Creating DB (if necessary) and running any migrations..."
   mix ecto.setup
+
+  echo "[*] Migrations finished successfully"
 fi
 
+echo "[*] Starting Phoenix server"
 mix phx.server
