d22c4380e4dcd42d4cd390519875db75ca48dbc1
Benjamin Porter <FreedomBen@users.noreply.github.com>
2021-04-26 17:30:54 -0600

    More progress toward prod k8s config

--- scripts/build-prod.sh
+++ scripts/build-prod.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-sudo podman build -f Dockerfile.prod -t docker.io/freedomben/malan-prod:latest .
+sudo podman build \
+  -f Dockerfile.prod \
+  -t docker.io/freedomben/malan-prod:latest \
+  .
