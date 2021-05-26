8e8996cfd7e9459c61939691325d287dbf440d3a
Benjamin Porter <FreedomBen@users.noreply.github.com>
2021-05-26 17:46:46 -0600

    Add curl script for getting current session

---
+++ scripts/curl/session_get_current.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# First get a token for the bootstrapped super user
+api_token="$(curl \
+               --request POST \
+               --header "Accept: application/json" \
+               --header "Content-Type: application/json" \
+               --data '{"session":{"email":"root@example.com","username":"root","password":"password10"}}' \
+               http://localhost:4000/api/sessions/ \
+              | jq -r '.data.api_token')"
+
+
+curl \
+  --request GET \
+  --header "Accept: application/json" \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Bearer ${api_token}" \
+  http://localhost:4000/api/sessions/current
+
