1b778de471f838fdfc35b4b0bcdea374bf0806f3
Benjamin Porter <FreedomBen@users.noreply.github.com>
2021-05-26 18:13:55 -0600

    Finish out session delete curl script

--- scripts/curl/session_delete.sh
+++ scripts/curl/session_delete.sh
@@ -23,43 +23,15 @@ curl \
   --header "Accept: application/json" \
   --header "Content-Type: application/json" \
   --header "Authorization: Bearer ${api_token}" \
-  http://localhost:4000/api/users/current
-  
+  http://localhost:4000/api/users/current \
+  >/dev/null
 
-# Now create the new user
-user_id="$(curl \
-  --request POST \
-  --header "Accept: application/json" \
-  --header "Content-Type: application/json" \
-  --header "Authorization: Bearer ${api_token}" \
-  --data "{\"user\":{\"email\":\"${NEW_EMAIL}\",\"username\":\"${NEW_USERNAME}\",\"password\":\"${NEW_PASSWORD}\",\"first_name\":\"${NEW_FIRST_NAME}\",\"last_name\":\"${NEW_LAST_NAME}\"}}" \
-  http://localhost:4000/api/users/ \
-  | jq -r '.data.id')"
 
-# Get the user's ID
-if [ "${user_id}" = 'null' ]; then
-  echo creating the user failed.  It probably already exists
-  exit 1
-fi
-
-#echo "api_token is: ${api_token}"
-#echo "user_id is: ${user_id}"
-
-# Now update the user
+# Now delete the current session
 curl \
-  --request PUT \
+  --request DELETE \
   --header "Accept: application/json" \
   --header "Content-Type: application/json" \
   --header "Authorization: Bearer ${api_token}" \
-  --data "{\"user\":{\"roles\":${NEW_ROLES}}}" \
-  http://localhost:4000/api/admin/users/${user_id}
-  #http://localhost:4000/api/users/${user_id}
-
-# Get the user to make sure it took hold
-curl \
-  --request GET \
-  --header "Accept: application/json" \
-  --header "Content-Type: application/json" \
-  --header "Authorization: Bearer ${api_token}" \
-  http://localhost:4000/api/users/${user_id}
+  http://localhost:4000/api/sessions/current
 
