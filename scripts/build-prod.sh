fce7829b1eeef84e979186a5d9365b41dbce5e35
Benjamin Porter <FreedomBen@users.noreply.github.com>
2021-08-18 12:04:20 -0600

    fix bug in prod build script

--- scripts/build-prod.sh
+++ scripts/build-prod.sh
@@ -4,5 +4,5 @@ LATEST_VERSION='2021-08-17a'
 
 docker build \
   -f Dockerfile.prod \
-  -t "docker.io/freedomben/${LATEST_VERSION}" \
+  -t "docker.io/freedomben/malan-prod:${LATEST_VERSION}" \
   .
