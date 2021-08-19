d21c3f7f9e50991008a057e30002d9f858a4de08
Benjamin Porter <FreedomBen@users.noreply.github.com>
2021-08-19 17:27:58 -0600

    Release version 20210819172734

--- scripts/push-prod.sh
+++ scripts/push-prod.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-LATEST_VERSION='20210819170954'
+LATEST_VERSION='20210819172734'
 
 docker push "docker.io/freedomben/malan-prod:${LATEST_VERSION}"
--- scripts/push-staging.sh
+++ scripts/push-staging.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-LATEST_VERSION='20210819170954'
+LATEST_VERSION='20210819172734'
 
 docker push "docker.io/freedomben/malan-staging:${LATEST_VERSION}"
