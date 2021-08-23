b84c92c746f9b9fe8885f3567a6a9b6fe3cda987
Benjamin Porter <FreedomBen@users.noreply.github.com>
2021-08-23 12:32:39 -0600

    Fix broken ARM build
    postgres doesn't have an aarch64 postgresql-client-12 build :-(

--- scripts/build-arm.sh
+++ scripts/build-arm.sh
@@ -3,7 +3,7 @@
 LATEST_VERSION='20210819172734'
 
 docker build \
-  -f Dockerfile.prod \
+  -f Dockerfile.arm \
   -t "docker.io/freedomben/malan-arm:${LATEST_VERSION}" \
   -t "docker.io/freedomben/malan-arm:latest" \
   .
