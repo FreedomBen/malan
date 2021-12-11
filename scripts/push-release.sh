ea28859d7f9b7c977be311619232662895479d95
Benjamin Porter <FreedomBen@users.noreply.github.com>
2021-12-11 15:38:24 -0700

    Fix push release script

--- scripts/push-release.sh
+++ scripts/push-release.sh
@@ -2,6 +2,9 @@
 
 if [ -z "${RELEASE_VERSION}" ]; then
   RELEASE_VERSION="$(git rev-parse HEAD)"
+  echo "RELEASE_VERSION is not set.  Setting to HEAD (${RELEASE_VERSION})"
+else
+  echo "RELEASE_VERSION already set to '${RELEASE_VERSION}'"
 fi
 
 docker push "docker.io/freedomben/malan:latest"
