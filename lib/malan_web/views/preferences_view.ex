8cb816ddf990451fc5edd4d48532da9e01c126e5
Benjamin Porter <FreedomBen@users.noreply.github.com>
2022-01-06 17:54:23 -0700

    Add middle name, name suffix, name prefix, display name to users

--- lib/malan_web/views/preferences_view.ex
+++ lib/malan_web/views/preferences_view.ex
@@ -2,6 +2,10 @@ defmodule MalanWeb.PreferencesView do
   use MalanWeb, :view
 
   def render("preferences.json", %{preferences: preferences}) do
-    %{theme: preferences.theme}
+    %{
+      theme: preferences.theme,
+      display_name_pref: preferences.display_name_pref,
+      display_middle_initial_only: preferences.display_middle_initial_only
+    }
   end
 end
--- priv/repo/migrations/20220106021608_add_middle_name_initial.exs
+++ priv/repo/migrations/20220106021608_add_middle_name_initial.exs
@@ -5,10 +5,10 @@ defmodule Malan.Repo.Migrations.AddMiddleNameInitial do
     alter table("users") do
       # suffix can be arbitrary:  https://en.wikipedia.org/wiki/Suffix_(name)
       # prfix can be arbitrary:  https://findanyanswer.com/what-does-name-prefix-mean
-      add :middle_name, :string, null: false, default: ""
-      add :name_suffix, :string, null: false, default: ""
-      add :name_prefix, :string, null: false, default: ""
-      add :display_name, :string, null: false, default: ""
+      add :middle_name, :string, null: true
+      add :name_suffix, :string, null: true
+      add :name_prefix, :string, null: true
+      add :display_name, :string, null: true
     end
   end
 end
