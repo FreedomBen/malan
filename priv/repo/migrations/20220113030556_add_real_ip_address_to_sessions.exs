3bfe9b8275e839970629c11abcb4f722ae4ba7d3
Benjamin Porter <FreedomBen@users.noreply.github.com>
2022-01-13 19:25:50 -0700

    Real IP address column should be nillable

--- priv/repo/migrations/20220113030556_add_real_ip_address_to_sessions.exs
+++ priv/repo/migrations/20220113030556_add_real_ip_address_to_sessions.exs
@@ -3,7 +3,7 @@ defmodule Malan.Repo.Migrations.AddRealIpAddressToSessions do
 
   def change do
     alter table("sessions") do
-      add :real_ip_address, :string, null: false
+      add :real_ip_address, :string, null: true
     end
   end
 end
