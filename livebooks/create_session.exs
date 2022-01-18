45f29e7434cf919e132e82abe1366ab4a187df1a
Benjamin Porter <FreedomBen@users.noreply.github.com>
2022-01-17 22:26:47 -0700

    Add some to livebooks

--- livebooks/create_session.exs
+++ livebooks/create_session.exs
@@ -1,9 +1,17 @@
+require Ecto.Query  # So we can use the `from` macro
+
+import Ecto.Query, only: [from: 2] # So we can use the from macro directly
+
 alias Malan.Repo
 alias Malan.Accounts
-alias Malan.Accounts.User
+alias Malan.Accounts.{User, Session, Transaction}
 
 # Retrieve the user's ID
 %User{id: user_id} = user = Accounts.get_user_by(email: "email@example.com")
 
 # Use the user ID to create a new session (API token)
 Accounts.new_session(%{"user_id" => user_id, "ip_address" => "127.0.0.1"})
+
+
+# Count number of sessions with a real_ip_address
+Repo.one(from(s in Session, select: count(s.id), where: not is_nil(s.real_ip_address)))
--- livebooks/query_transactions.exs
+++ livebooks/query_transactions.exs
@@ -4,7 +4,7 @@ import Ecto.Query, only: [from: 2] # So we can use the from macro directly
 
 alias Malan.Repo
 alias Malan.Accounts
-alias Malan.Accounts.{User, Transaction}
+alias Malan.Accounts.{User, Session, Transaction}
 
 # Retrieve the Count of Transactions
 Ecto.Query.from(t in Transaction, select: count(t.id))
@@ -15,5 +15,5 @@ from(t in Transaction, select: t, limit: 5, order_by: t.inserted_at)
 |> Repo.all()
 
 # Retrieve 5 most recent transactions
-from(t in Transaction, select: t, limit: 5, order_by: [desc: t.inserted_at])
+from(t in Transaction, select: t, limit: 5, order_by: [desc_nulls_last: t.inserted_at])
 |> Repo.all()
