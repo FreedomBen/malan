64b28dc376fad273d715d29ae9b9adc2a1da8aba
Benjamin Porter <FreedomBen@users.noreply.github.com>
2022-02-16 09:04:34 -0700

    preset url to avoid risk of typos

--- lib/malan_web/notifiers/user_notifier.ex
+++ lib/malan_web/notifiers/user_notifier.ex
@@ -5,21 +5,26 @@ defmodule MalanWeb.UserNotifier do
 
   def welcome_and_confirm_email(user) do
     new()
-    #|> to({user.name, user.email})
+    # |> to({user.name, user.email})
     |> to(user)
     |> from({"Malan", "noreply@example.com"})
     |> subject("Hello, Avengers!")
     |> render_body("welcome_confirm_email.html", %{name: user.name})
-    #|> render_html("<h1>Hello</h1>")
+
+    # |> render_html("<h1>Hello</h1>")
   end
 
   def password_reset_email(user) do
     new()
     |> to(user)
-    |> from({"Malan", "noreply@example.com"})
-    |> subject("Password Reset Token")
-    |> render_body("password_reset_email.html", %{user: user})
-    #password_reset_token: user.password_reset_token,
-    #password_reset_token_expires_at: user.password_reset_token_expires_at
+    |> from({"Ameelio", "noreply@ameelio.org"})
+    |> subject("Your requested password reset token")
+    |> render_body("password_reset_email.html", %{
+      user: user,
+      url: "https://ameelio.org/password/reset/#{user.password_reset_token}"
+    })
+
+    # password_reset_token: user.password_reset_token,
+    # password_reset_token_expires_at: user.password_reset_token_expires_at
   end
 end
