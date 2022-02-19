a49aabf03ea954da81ee8fa13bb0218e2dff02d0
Benjamin Porter <FreedomBen@users.noreply.github.com>
2022-02-18 18:15:46 -0700

    Fix endpoint in session create script

--- scripts/curl/session_create.sh
+++ scripts/curl/session_create.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
+#MALAN_ENDPOINT="http://localhost:4000"
+MALAN_ENDPOINT="https://malan-staging.ameelio.org"
+#MALAN_ENDPOINT="https://malan.ameelio.org"
+
 # Default attributes
 #curl \
 #  --request POST \
 #  --header "Accept: application/json" \
 #  --header "Content-Type: application/json" \
 #  --data '{"session":{"email":"root@example.com","username":"root","password":"password10"}}' \
-#  http://localhost:4000/api/sessions/
+#  "${MALAN_ENDPOINT}/api/sessions/"
 
 # Forever token
 curl \
@@ -15,7 +19,7 @@ curl \
   --header "Content-Type: application/json" \
   --header "CF-Connecting-IP: 127.0.1.1" \
   --data '{"session":{"email":"root@example.com","username":"root","password":"password10","never_expires":true}}' \
-  http://localhost:4000/api/sessions/
+  "${MALAN_ENDPOINT}/api/sessions/"
 
 # 30 second token
 #curl \
@@ -23,4 +27,4 @@ curl \
 #  --header "Accept: application/json" \
 #  --header "Content-Type: application/json" \
 #  --data '{"session":{"email":"root@example.com","username":"root","password":"password10","expires_in_seconds":30}}' \
-#  http://localhost:4000/api/sessions/
+#  "${MALAN_ENDPOINT}/api/sessions/"
