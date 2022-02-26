eee4c3be4188e8225f68ed8164b45765c665a8c1
Benjamin Porter <FreedomBen@users.noreply.github.com>
2022-02-25 20:13:17 -0700

    Allow success column in Transaction to be null
    Otherwise all the existing records will bomb in the migration.
    
    We still require changesets to include a value

--- priv/repo/migrations/20220220023537_add_success_to_transactions.exs
+++ priv/repo/migrations/20220220023537_add_success_to_transactions.exs
@@ -3,7 +3,25 @@ defmodule Malan.Repo.Migrations.AddSuccessToTransactions do
 
   def change do
     alter table("transactions") do
-      add :success, :boolean, null: false
+      add :success, :boolean, null: true
     end
   end
+
+  # Initially we were going to require non-null but initialize existing records:
+  #
+  # def up do
+  #   alter table(:transactions) do
+  #     add :success, :boolean, default: true, null: false
+  #   end
+  #
+  #   alter table(:transactions) do
+  #     modify :success, :boolean, default: nil, null: false
+  #   end
+  # end
+
+  # def down do
+  #   alter table(:transactions) do
+  #     remove :success,  :boolean, null: false
+  #   end
+  # end
 end
