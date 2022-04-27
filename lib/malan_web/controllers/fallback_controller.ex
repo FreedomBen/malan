ff7bd9f2c5c4b21dadfecc8161866cbac0007ce6
Benjamin Porter <FreedomBen@users.noreply.github.com>
2022-04-26 21:40:09 -0600

    Make 422 errors with changeset info more consistent
    Without throwing away useful data for the user.  This also removes the
    errors attribute and makes the children root level members.
    
    Improves response when pagination is incorrect.
    
    Updates all tests and fix a few bugs ran into
    
    Fixes #99
    Fixes FreedomBen/libmalan#24

--- lib/malan_web/controllers/fallback_controller.ex
+++ lib/malan_web/controllers/fallback_controller.ex
@@ -9,24 +9,26 @@ defmodule MalanWeb.FallbackController do
   """
   use MalanWeb, :controller
 
+  import MalanWeb.ChangesetView, only: [translate_errors: 1]
+
   def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
     conn
     |> put_status(:unprocessable_entity)
-    |> put_view(MalanWeb.ChangesetView)
-    |> render("error.json", changeset: changeset)
+    |> put_view(MalanWeb.ErrorView)
+    |> render("422.json", errors: translate_errors(changeset))
   end
 
   def call(conn, {:error, :not_found}) do
     conn
     |> put_status(:not_found)
     |> put_view(MalanWeb.ErrorView)
-    |> render(:"404")
+    |> render("404.json")
   end
 
   def call(conn, {:error, :too_many_requests}) do
     conn
     |> put_status(:too_many_requests)
     |> put_view(MalanWeb.ErrorView)
-    |> render(:"429")
+    |> render("429.json")
   end
 end
