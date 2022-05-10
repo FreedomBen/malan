f5d09d54b15a7286459f520709dfe33cd2417564
Benjamin Porter <FreedomBen@users.noreply.github.com>
2022-05-10 17:10:48 -0600

    Add birthday query livebook example

---
+++ livebooks/query_birthdays.exs
@@ -0,0 +1,15 @@
+require Ecto.Query  # So we can use the `from` macro
+
+import Ecto.Query, only: [from: 2] # So we can use the from macro directly
+
+alias Malan.Repo
+alias Malan.Accounts
+alias Malan.Accounts.{User, Session, Transaction}
+
+#from(u in User, select: count(u.id))
+from(u in User, where: not is_nil(u.birthday), limit: 10)
+|> Repo.all()
+
+from(u in User, select: count(u.id), where: not is_nil(u.birthday))
+|> Repo.one()
+
