e5900676a2e7f99b08125a9cc896ae1d97d64531
Benjamin Porter <FreedomBen@users.noreply.github.com>
2022-07-31 16:29:04 -0600

    Update livebook script

--- livebooks/change_user_password.exs
+++ livebooks/change_user_password.exs
@@ -4,11 +4,9 @@ alias Malan.Accounts.User
 
 # Retrieve the user's ID
 %User{id: user_id} = user = Accounts.get_user_by(email: "email@example.com")
-%User{id: user_id} = user = Accounts.get_user_by(email: "bradleyc61@yahoo.com")
-%User{id: user_id} = u3 = Accounts.get_user_by(email: "chloeray_05@yahoo.com")
 
 # Change user's password
-Accounts.update_user(user, %{"password" => "2YzMtYzdmN2"})
+Accounts.update_user(user, %{"password" => "password10"})
 
 # Lock the user's account
 Accounts.lock_user(user, nil)
