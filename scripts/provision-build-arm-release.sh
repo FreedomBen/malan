bc1c15bcf014725b5d6f1a0e7efbd86f6b86c0a9
Benjamin Porter <FreedomBen@users.noreply.github.com>
2022-08-03 14:55:48 -0600

    Improve documentation on arm provisioning script

--- scripts/provision-build-arm-release.sh
+++ scripts/provision-build-arm-release.sh
@@ -1,35 +1,59 @@
 #!/usr/bin/env bash
 
 #
-# This script is meant to run on a fresh Ubuntu ARM64 ec2 instance.
-# It will build and push malan latest arm releases
+# 1.  Provision the ARM machine
 #
-# To find the image ID (not this takes a LONG time it's 262MB!):
-#   aws ec2 describe-images | jq -r ".Images" > images.json
+#   A.  Part 1 will get you the ID for the AMI, which you need pass to `aws ec2 run-instances ...`
+#   B.  Part 2 uses the ID for the AMI to create the things
 #
-# To use irb:
-#   irb
-#   require 'json'
-#   require 'date'
-#   puts JSON.parse(File.read("images.json"))
-#      .filter{|i| i["OwnerId"] == "099720109477" && i["Architecture"] == "arm64" && i["State"] == "available" && i["Name"] =~ /ubuntu-jammy/i && i["Name"] =~ /^ubuntu\/images\//i}
-#      .sort{|i1, i2| DateTime.parse(i2["CreationDate"]) <=> DateTime.parse(i1["CreationDate"])}
-#      .map{|i| "#{i["ImageId"]} - #{i["Name"]}"}
-#      .join("\n")
+# 2.  Run this script to build and push the images
 #
-# To use ruby from the CLI:
-#   ruby -r 'json' -r 'date' -e 'puts JSON.parse(File.read("images.json")).filter{|i| i["OwnerId"] == "099720109477" && i["Architecture"] == "arm64" && i["State"] == "available" && i["Name"] =~ /ubuntu-jammy/i && i["Name"] =~ /^ubuntu\/images\//i}.sort{|i1, i2| DateTime.parse(i2["CreationDate"]) <=> DateTime.parse(i1["CreationDate"])}.map{|i| "#{i["ImageId"]} - #{i["Name"]}"}.join("\n")'
+#
+# Part 1 (getting the ID for the AMI)
+#
+#  Capture the JSON into a file because it's huge
+#
+#   To find the image ID (not this takes a LONG time it's 262MB!):
+#     aws ec2 describe-images | jq -r ".Images" > images.json
+#
+#   Best way to extract the image ID:
+#
+#     ruby -r 'json' -r 'date' -e 'puts JSON.parse(File.read("images.json")).filter{|i| i["OwnerId"] == "099720109477" && i["Architecture"] == "arm64" && i["State"] == "available" && i["Name"] =~ /ubuntu-jammy/i && i["Name"] =~ /^ubuntu\/images\//i}.sort{|i1, i2| DateTime.parse(i2["CreationDate"]) <=> DateTime.parse(i1["CreationDate"])}.first.to_json' | jq -r '.ImageId'
+#
+#   Notes from irb usage (kept for reference):
+#
+#     irb
+#     require 'json'
+#     require 'date'
+#     puts JSON.parse(File.read("images.json"))
+#        .filter{|i| i["OwnerId"] == "099720109477" && i["Architecture"] == "arm64" && i["State"] == "available" && i["Name"] =~ /ubuntu-jammy/i && i["Name"] =~ /^ubuntu\/images\//i}
+#        .sort{|i1, i2| DateTime.parse(i2["CreationDate"]) <=> DateTime.parse(i1["CreationDate"])}
+#        .map{|i| "#{i["ImageId"]} - #{i["Name"]}"}
+#        .join("\n")
+#
+#   To use ruby from the CLI to parse the AMI ID (kept for reference):
+#
+#     ruby -r 'json' -r 'date' -e 'puts JSON.parse(File.read("images.json")).filter{|i| i["OwnerId"] == "099720109477" && i["Architecture"] == "arm64" && i["State"] == "available" && i["Name"] =~ /ubuntu-jammy/i && i["Name"] =~ /^ubuntu\/images\//i}.sort{|i1, i2| DateTime.parse(i2["CreationDate"]) <=> DateTime.parse(i1["CreationDate"])}.map{|i| "#{i["ImageId"]} - #{i["Name"]}"}.join("\n")'
 
-# To get the image ID:
 #
-#   ruby -r 'json' -r 'date' -e 'puts JSON.parse(File.read("images.json")).filter{|i| i["OwnerId"] == "099720109477" && i["Architecture"] == "arm64" && i["State"] == "available" && i["Name"] =~ /ubuntu-jammy/i && i["Name"] =~ /^ubuntu\/images\//i}.sort{|i1, i2| DateTime.parse(i2["CreationDate"]) <=> DateTime.parse(i1["CreationDate"])}.first.to_json' | jq -r '.ImageId'
+#     # aws ec2 run-instances --image-id ami-0ee02425a4c7e78bb --count 1 --instance-type c6g.xlarge --key-name ben_0 --security-group-ids sg-903004f8 --subnet-id subnet-6e7f829e
 #
 #
-# # aws ec2 run-instances --image-id ami-0ee02425a4c7e78bb --count 1 --instance-type c6g.xlarge --key-name ben_0 --security-group-ids sg-903004f8 --subnet-id subnet-6e7f829e
 #
+# Once you have the ami's image id, stick in variable:
+#
+#######  Running all the things
+#
+# # save images.json locally if don't have it
+# aws ec2 describe-images | jq -r ".Images" > images.json
+#
+# # If need to extract the AMI ID.  See next if alreay have it
 # imageid="$(ruby -r 'json' -r 'date' -e 'puts JSON.parse(File.read("images.json")).filter{|i| i["OwnerId"] == "099720109477" && i["Architecture"] == "arm64" && i["State"] == "available" && i["Name"] =~ /ubuntu-jammy/i && i["Name"] =~ /^ubuntu\/images\//i}.sort{|i1, i2| DateTime.parse(i2["CreationDate"]) <=> DateTime.parse(i1["CreationDate"])}.first.to_json' | jq -r '.ImageId')"
+# # If already have it:
+# imageid="ami-0ee02425a4c7e78bb"
 #
-# aws ec2 run-instances --image-id ami-0ee02425a4c7e78bb --count 1 --instance-type c6g.xlarge --key-name ben_0
+# # Create the ec2 instance
+# aws ec2 run-instances --image-id "${imageid}" --count 1 --instance-type c6g.xlarge --key-name ben_0
 # # wait for instance to initialize and get a public IP
 # # aws ec2 describe-instances | jq -r '.Reservations | map(.Instances) | map(.PublicIpAddress)'
 # # Get the public IP of the machine
@@ -50,6 +74,13 @@
 #   sudo ./provision-build-arm-release.sh
 # # Destroy the instance
 # aws ec2 terminate-instances --instance-ids "${iid}"
+
+
+
+###
+# This script is meant to run on a fresh Ubuntu ARM64 ec2 instance.
+# It will build and push malan latest arm releases
+###
 
 set -e
 
