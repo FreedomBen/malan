ecc799d4efba49ba89a5d8449d424dad7fc9987d
Benjamin Porter <FreedomBen@users.noreply.github.com>
2022-09-15 17:31:22 -0600

    Remove extra origins

--- config/prod.exs
+++ config/prod.exs
@@ -10,12 +10,7 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :malan, MalanWeb.Endpoint,
-  cache_static_manifest: "priv/static/cache_manifest.json",
-  check_origin: [
-    "https://malan.ameelio.org",
-    "https://malan-prod.ameelio.org",
-    "https://accounts.ameelio.org"
-  ]
+  cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info
