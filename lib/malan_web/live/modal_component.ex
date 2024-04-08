ece7495817b1daa9406f3f7ec35334465aa217fb
Benjamin Porter <FreedomBen@users.noreply.github.com>
2024-04-08 16:17:03 -0600

    Update LiveView to 0.20

--- lib/malan_web/live/modal_component.ex
+++ lib/malan_web/live/modal_component.ex
@@ -4,21 +4,25 @@ defmodule MalanWeb.ModalComponent do
   @impl true
   def render(assigns) do
     ~H"""
-    <div
-      id={@id}
-      class="phx-modal"
-      phx-capture-click="close"
-      phx-window-keydown="close"
-      phx-key="escape"
-      phx-target={@myself}
-      phx-page-loading>
-
-      <div class="phx-modal-content">
-        <%= live_patch raw("&times;"), to: @return_to, class: "phx-modal-close" %>
-        <%= live_component @component, @opts %>
-      </div>
-    </div>
+      If you want to use this component, you need to update the live_patch and live_component 
+      functions below.  See:  https://hexdocs.pm/phoenix_live_view/changelog.html#0-18-0-2022-09-20
     """
+    # ~H"""
+    # <div
+    #   id={@id}
+    #   class="phx-modal"
+    #   phx-capture-click="close"
+    #   phx-window-keydown="close"
+    #   phx-key="escape"
+    #   phx-target={@myself}
+    #   phx-page-loading>
+
+    #   <div class="phx-modal-content">
+    #     <%= live_patch raw("&times;"), to: @return_to, class: "phx-modal-close" %>
+    #     <%= live_component @component, @opts %>
+    #   </div>
+    # </div>
+    # """
   end
 
   @impl true
